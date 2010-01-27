30cd34484c4562076899ea00d3d0adcb1505c384
tafryn <tafryn@gmail.com>
2010-01-27 10:24:17 -0500

    4.11

--- data.hs
+++ data.hs
@@ -23,40 +23,40 @@ firstTwo (Quadruple a b c d) = [a, b]
 lastTwo :: (Quadruple a b) -> [b]
 lastTwo (Quadruple a b c d) = [c, d]
 
-data My_Tuple a b c d = One a
-                      | Two a b
-                      | Three a b c
-                      | Four a b c d
+data MyTuple a b c d = One a
+                     | Two a b
+                     | Three a b c
+                     | Four a b c d
 
-tuple1 :: (My_Tuple a b c d) -> Maybe a
+tuple1 :: (MyTuple a b c d) -> Maybe a
 tuple1 (One a       ) = Just a
 tuple1 (Two a b     ) = Just a
 tuple1 (Three a b c ) = Just a
 tuple1 (Four a b c d) = Just a
 
-tuple2 :: (My_Tuple a b c d) -> Maybe b
+tuple2 :: (MyTuple a b c d) -> Maybe b
 tuple2 (One a       ) = Nothing
 tuple2 (Two a b     ) = Just b
 tuple2 (Three a b c ) = Just b
 tuple2 (Four a b c d) = Just b
 
-tuple3 :: (My_Tuple a b c d) -> Maybe c
+tuple3 :: (MyTuple a b c d) -> Maybe c
 tuple3 (One a       ) = Nothing
 tuple3 (Two a b     ) = Nothing
 tuple3 (Three a b c ) = Just c
 tuple3 (Four a b c d) = Just c
 
-tuple4 :: (My_Tuple a b c d) -> Maybe d
+tuple4 :: (MyTuple a b c d) -> Maybe d
 tuple4 (One a       ) = Nothing
 tuple4 (Two a b     ) = Nothing
 tuple4 (Three a b c ) = Nothing
 tuple4 (Four a b c d) = Just d
 
-tuple_translate :: (My_Tuple a b c d) -> Either a (Either (a,b) (Either (a,b,c) (a,b,c,d)))
-tuple_translate (One a       ) = Left a
-tuple_translate (Two a b     ) = Right (Left (a,b))
-tuple_translate (Three a b c ) = Right (Right (Left (a,b,c)))
-tuple_translate (Four a b c d) = Right (Right (Right (a,b,c,d)))
+tupleTranslate :: (MyTuple a b c d) -> Either a (Either (a,b) (Either (a,b,c) (a,b,c,d)))
+tupleTranslate (One a       ) = Left a
+tupleTranslate (Two a b     ) = Right (Left (a,b))
+tupleTranslate (Three a b c ) = Right (Right (Left (a,b,c)))
+tupleTranslate (Four a b c d) = Right (Right (Right (a,b,c,d)))
 
 
 data List a = Nil
@@ -76,5 +76,44 @@ listFoldr f i (Cons x xs) = listFoldr f (f x i) xs
 listFoldl :: (a -> b -> a) -> a -> List b -> a
 listFoldl f i Nil = i
 listFoldl f i (Cons x xs) = listFoldl f (f i x) xs
+
+data BinaryTree a = Leaf a
+                  | Branch (BinaryTree a) a (BinaryTree a)
+
+treeSize (Leaf x) = 1
+treeSize (Branch left x right) = 1 + treeSize(left) + treeSize(right)
+
+btElements (Leaf x) = [x]
+btElements (Branch left x right) = btElements(left) ++ [x] ++ btElements(right)
+
+treeFold :: (a -> b -> b) -> b -> (BinaryTree a) -> b
+treeFold f i (Leaf x) = f x i
+treeFold f i (Branch left x right) = treeFold f (f x (treeFold f i left)) right 
+
+btElements2 x = treeFold (\y z -> z ++ [y]) [] x
+
+data Color
+    = Red
+    | Green
+    | Blue
+
+colorToRGB Red   = (255,0,0)
+colorToRGB Green = (0,255,0)
+colorToRGB Blue  = (0,0,255)
+
+cfold' f z [] = z
+cfold' f z (x:xs) = f x z (\y -> cfold' f y xs)
+
+cfold f z l = cfold' (\x t g -> f x (g t)) z l
+
+cmap' f [] = []
+cmap' f (x:xs) = f x (cmap' f xs)
+
+cmap f lst = cmap' (\x y -> (f x) : y) lst
+
+cfilter' f [] = []
+cfilter' f (x:xs) = f x (cfilter' f xs)
+
+cfilter f lst = cfilter' (\x y -> if (f x) then x : y else y) lst
 
 -- vim: ts=4 sw=4 et:
